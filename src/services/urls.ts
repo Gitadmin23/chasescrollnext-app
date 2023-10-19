375a189474922f4b9087e049eede5ad2cbb94ce2
daniel <danielemmanuel257@gmail.com>
2023-10-19 12:07:31 +0100

    finished the home page

--- src/services/urls.ts
+++ src/services/urls.ts
@@ -39,28 +39,15 @@ export const URLS = {
     PAST_EVENT: "/events/get-past-events",
     SAVED_EVENT: "/events/get-saved-events?typeID="
 }
+
+export const IMAGE_URL = process.env.NEXT_PUBLIC_RESOURCE_URL;
     CREATE_REPORT: '/report/report',
-    REMOVE_FRIEND: '/user/remove-friend/',
-    ADD_FRIEND: '/user/send-friend-request',
-    BLOCK_USER: '/user/block',
-    GET_SUGGESTED_FRIENDS: "/user/suggest-connections",
-    JOIN_GROUP: "/group/join-group",
-    LEAVE_GROUP: "/group/leave-group",
-    EVENT_TYPE: "/events/get-event-types",
-    JOINED_EVENT: "/events/joined-events/",
-    All_EVENT: "/events/events",
-    CREATE_TICKET: "/events/create-ticket",
-    GET_EVENTS_TYPES: "/events/get-event-types",
-    CREATE_DRAFT: "/events/create-draft",
-    UPDATE_DRAFT: "/events/update-draft",
-    CREATE_EVENT_FROM_DRAFT: "/events/create-event-from-draft",
-    GET_DRAFT: "/events/drafts",
-    PAST_EVENT: "/events/get-past-events",
-    SAVED_EVENT: "/events/get-saved-events?typeID="
+    JOINED_GROUPS: '/group/joined-groups',
+    FIND_GROUPS: '/group/find-groups',
+    GROUP_GROUP: '/group/group',
+    GET_GROUP_REQUESTS: '/group/get-requests',
+    CREATE_COMMUNITY: '/group/create-group',
 }
 
-export const IMAGE_URL = process.env.NEXT_PUBLIC_RESOURCE_URL;
-
-export const IMAGE_URL = process.env.NEXT_PUBLIC_RESOURCE_URL;
-
+export const RESOURCE_BASE_URL = process.env.NEXT_PUBLIC_RESOURCE_URL;
 export const RESOURCE_BASE_URL = process.env.NEXT_PUBLIC_RESOURCE_URL;
