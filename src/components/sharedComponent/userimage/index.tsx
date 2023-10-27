de9d6ac54325b3f4cf0fd451968c76f2097e1349
egileekada <egileoniso.ekada@gmail.com>
2023-10-27 02:31:30 +0100

    added reuseable component to the sharedcomponent folder

---
+++ src/components/sharedComponent/userimage/index.tsx
@@ -0,0 +1,32 @@
+import { IMAGE_URL } from '@/services/urls'
+import { Box, Flex, Image, Text } from '@chakra-ui/react' 
+import React from 'react'
+
+interface Props {
+    data: any,
+    size: number,
+    font?: string
+}
+
+function UserImage(props: Props) {
+    const {
+        data,
+        size,
+        font
+    } = props
+
+    return (
+        <Box roundedBottom={"64px"} roundedTopLeft={"64px"} borderColor={"#D0D4EB"} w={size} h={size} borderWidth={"5px"} >
+            {data?.data?.imgMain?.value &&
+                <Image style={{ borderBottomLeftRadius: "64px", borderBottomRightRadius: "64px", borderTopLeftRadius: "64px" }} objectFit="cover" alt={data?.data?.imgMain?.value} width={"full"} height={"full"} src={IMAGE_URL + data?.data?.imgMain?.value} />
+            }
+            {!data?.data?.imgMain?.value && (
+                <Flex justifyContent={"center"} alignItems={"center"} width={"full"} height={"full"} fontSize={font ? font : "30px"} fontWeight={"bold"} >
+                    <Text>{data?.firstName?.charAt(0).toUpperCase()}{data?.lastName?.charAt(0).toUpperCase()}</Text>
+                </Flex> 
+            )}
+        </Box> 
+    )
+}
+
+export default UserImage
