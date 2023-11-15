1704d5f9d66442106eede1b6e81ddb8ed5d21ea4
egileekada <egileoniso.ekada@gmail.com>
2023-11-15 21:23:11 +0100

    update on bugs

--- src/components/profile_component/index.tsx
+++ src/components/profile_component/index.tsx
@@ -6,9 +6,10 @@ import ProfileHeader from './profile_header'
 import InfiniteScrollerComponent from '@/hooks/infiniteScrollerComponent'
 import { IMAGE_URL, URLS } from '@/services/urls'
 import PostThreads from './post_component'
+import LoadingAnimation from '../sharedComponent/loading_animation'
 
 interface Props {
-    user_index: string| number
+    user_index: string | number
 }
 
 function ProfileComponent(props: Props) {
@@ -20,49 +21,51 @@ function ProfileComponent(props: Props) {
     const [postId, setPostId] = React.useState("")
     const { results, isLoading, ref, isRefetching, data } = InfiniteScrollerComponent({ url: URLS.GET_MEDIA_POST + user_index, limit: 10, filter: "id" })
 
-    const clickHandler =(item: string)=> {
+    const clickHandler = (item: string) => {
         setPostId(item)
         setOpen(true)
     }
 
     return (
-        <Flex width={"full"} justifyContent={"center"} py={"6"} px={"6"} >
-            <Grid width={"full"} templateColumns={['repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)', 'repeat(4, 1fr)']} gap={6} >
-                {results.filter((item: any) => item?.joinStatus !== "SELF")?.map((item: {
-                    type: string,
-                    mediaRef: string,
-                    id: string
-                }, index: number) => {
-                    return (
-                        <GridItem onClick={()=> clickHandler(item?.id)} as={"button"} key={index} width={"full"} height={"200px"} rounded={"24px"} roundedTopRight={"none"} bgColor={"gray.300"}  >
-                            {item?.type === "WITH_IMAGE" && (
-                                <>
-                                    { item?.mediaRef.startsWith('https://') && (
-                                        <Image src={`${item?.mediaRef}`} alt={item?.mediaRef} rounded={"24px"} roundedTopRight={"none"} shadow={"lg"} width={"full"} height={"full"} objectFit={"cover"} />
-                                    )}
-                                    { !item?.mediaRef.startsWith('https://') && (
-                                        <Image src={`${IMAGE_URL}${item?.mediaRef}`} alt={item?.mediaRef} rounded={"24px"} roundedTopRight={"none"} shadow={"lg"} width={"full"} height={"full"} objectFit={"cover"} />
-                                    )}
-                                </>
-                            )} 
-                            {item.type === 'WITH_VIDEO_POST' && item?.mediaRef &&
-                                <video
-                                    key={index.toString()}
-                                    style={{width: "100%", height: "100%", borderRadius: "24px", borderTopRightRadius: "0px"}}
-                                    className="rounded-b-[32px] rounded-tl-24px w-[170px] h-[170px] object-cover cursor-pointer z-0"
-                                    // alt="media from user post" 
-                                    controls
-                                    autoPlay={false}
-                                >
-                                    <source src={`${IMAGE_URL}${item?.mediaRef}`} type="video/mp4" />
-                                </video>
-                            }
-                        </GridItem>
-                    )
-                })}
-            </Grid>
-            <PostThreads post_index={postId} open={open} setOpen={setOpen} user_index={user_index} />
-        </Flex>
+        <LoadingAnimation loading={isLoading} refeching={isRefetching} length={results?.length} >
+            <Flex width={"full"} justifyContent={"center"} py={"6"} px={"6"} >
+                <Grid width={"full"} templateColumns={['repeat(2, 1fr)', 'repeat(3, 1fr)', 'repeat(4, 1fr)', 'repeat(4, 1fr)']} gap={6} >
+                    {results.filter((item: any) => item?.joinStatus !== "SELF")?.map((item: {
+                        type: string,
+                        mediaRef: string,
+                        id: string
+                    }, index: number) => {
+                        return (
+                            <GridItem onClick={() => clickHandler(item?.id)} as={"button"} key={index} width={"full"} height={"200px"} rounded={"24px"} roundedTopRight={"none"} bgColor={"gray.300"}  >
+                                {item?.type === "WITH_IMAGE" && (
+                                    <>
+                                        {item?.mediaRef.startsWith('https://') && (
+                                            <Image src={`${item?.mediaRef}`} alt={item?.mediaRef} rounded={"24px"} roundedTopRight={"none"} shadow={"lg"} width={"full"} height={"full"} objectFit={"cover"} />
+                                        )}
+                                        {!item?.mediaRef.startsWith('https://') && (
+                                            <Image src={`${IMAGE_URL}${item?.mediaRef}`} alt={item?.mediaRef} rounded={"24px"} roundedTopRight={"none"} shadow={"lg"} width={"full"} height={"full"} objectFit={"cover"} />
+                                        )}
+                                    </>
+                                )}
+                                {item.type === 'WITH_VIDEO_POST' && item?.mediaRef &&
+                                    <video
+                                        key={index.toString()}
+                                        style={{ width: "100%", height: "100%", borderRadius: "24px", borderTopRightRadius: "0px" }}
+                                        className="rounded-b-[32px] rounded-tl-24px w-[170px] h-[170px] object-cover cursor-pointer z-0"
+                                        // alt="media from user post" 
+                                        controls
+                                        autoPlay={false}
+                                    >
+                                        <source src={`${IMAGE_URL}${item?.mediaRef}`} type="video/mp4" />
+                                    </video>
+                                }
+                            </GridItem>
+                        )
+                    })}
+                </Grid>
+                <PostThreads post_index={postId} open={open} setOpen={setOpen} user_index={user_index} />
+            </Flex>
+        </LoadingAnimation>
     )
 }
 
--- src/components/sharedComponent/chat_btn/index.tsx
+++ src/components/sharedComponent/chat_btn/index.tsx
@@ -1,6 +1,9 @@
 import { MessageIcon } from '@/components/svg';
+import { Chat } from '@/models/Chat';
+import { WEBSITE_URL } from '@/services/urls';
 import httpService from '@/utils/httpService';
 import { Box, Spinner } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { useMutation } from 'react-query';
 
@@ -15,29 +18,49 @@ function ChatBtn(props: Props) {
         userId
     } = props
 
+    const router = useRouter()
 
-    const { isLoading: createChatLoading, mutate } = useMutation({
-        mutationFn: () => httpService.post('/chat/chat', {
-            image: `${profile?.data?.imgMain?.value}`,
-            name: profile?.username,
+    const { isLoading: chatCreationLoading, mutate } = useMutation({
+        mutationFn: () => httpService.post(`/chat/chat`, {
             type: 'ONE_TO_ONE',
-            typeID: userId,
             users: [
                 userId,
             ]
         }),
         onSuccess: (data) => {
-            // navigate(`/message?messageId=${data.data.id}`);
-        },
-        onError: (errror) => {
-            // toast.error('An error occured whilw trying to initiate chat');
+            const chat = data?.data as Chat;
+            const obj = {
+                message: `${WEBSITE_URL}/share?type=ONE_TO_ONE&typeID=${userId}`,
+                chatID: chat?.id,
+            }
+            router.replace(`/dashboard/chats?activeID=${obj?.chatID}`)
+            // sendMessage.mutate(obj)
         }
-    })
+
+    });
+
+    // const { isLoading: createChatLoading, mutate } = useMutation({
+    //     mutationFn: () => httpService.post('/chat/chat', {
+    //         image: `${profile?.data?.imgMain?.value}`,
+    //         name: profile?.username,
+    //         type: 'ONE_TO_ONE',
+    //         typeID: userId,
+    //         users: [
+    //             userId,
+    //         ]
+    //     }),
+    //     onSuccess: (data) => {
+    //         // navigate(`/message?messageId=${data.data.id}`);
+    //     },
+    //     onError: (errror) => {
+    //         // toast.error('An error occured whilw trying to initiate chat');
+    //     }
+    // })
 
     return (
         <Box as='button' onClick={()=> mutate()} > 
-            {createChatLoading && <Spinner colorScheme="black" />}
-            {!createChatLoading && 
+            {chatCreationLoading && <Spinner colorScheme="black" />}
+            {!chatCreationLoading && 
             <MessageIcon />}
         </Box>
     )
