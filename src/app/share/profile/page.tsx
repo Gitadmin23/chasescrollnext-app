0ee3f44928723772fd04474be90051297d7abe9b
egileekada <egileoniso.ekada@gmail.com>
2023-11-15 21:23:11 +0100

    update on bugs

--- src/app/share/profile/page.tsx
+++ src/app/share/profile/page.tsx
@@ -2,13 +2,20 @@
 import React from 'react'
 import { useSearchParams } from 'next/navigation'
 import GetProfile from '@/app/dashboard/profile/get_profile'
+import ProfileHeader from '@/components/profile_component/profile_header';
+import ProfileImage from '@/components/profile_component/profile_image';
+import { Box } from '@chakra-ui/react';
 
 function ShareProfile() {
-    const query = useSearchParams();
-    const type = query?.get('type');
-    const typeID = query?.get('typeID');
+  const query = useSearchParams();
+  const type = query?.get('type');
+  const typeID: any = query?.get('typeID');
   return (
-    <GetProfile profile_index={typeID} />
+    <Box width={"full"} overflowY={"auto"} >
+      <ProfileImage user_index={typeID} />
+      <ProfileHeader user_index={typeID} />
+      <GetProfile profile_index={typeID} />
+    </Box>
   )
 }
 
