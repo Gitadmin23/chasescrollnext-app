5ee4942ed2edc433d79b2ebe729c66c99760bb96
egileekada <egileoniso.ekada@gmail.com>
2023-12-06 22:31:37 +0100

    fixing share google login

--- src/app/share/auth/login/page.tsx
+++ src/app/share/auth/login/page.tsx
@@ -14,8 +14,9 @@ import httpService from '@/utils/httpService';
 import { URLS } from '@/services/urls';
 import { CustomInput } from '@/components/Form/CustomInput';
 import { signIn, useSession } from 'next-auth/react'
-import { Session,  } from 'next-auth';
+import { Session, } from 'next-auth';
 import { useShareState } from '../../state';
+import httpServiceGoogle from '@/utils/httpServiceGoogle';
 
 
 
@@ -37,7 +38,7 @@ function LoginPage() {
   const [FirstName, setFirstName] = React.useState("")
   const [CheckUsername, setCheckUsername] = React.useState("")
   const [LastName, setLastName] = React.useState("")
-  const [UserName, setUserName] = React.useState("") 
+  const [UserName, setUserName] = React.useState("")
   const [checkData, setCheckData] = React.useState<any>({})
 
 
@@ -50,10 +51,10 @@ function LoginPage() {
 
   const handleSignIn = async (event: any) => {
     if (sessionData !== null) {
-        const id: string = (sessionData as any)['idToken'];
-        signinWithGoogle.mutate(id);
+      const id: string = (sessionData as any)['idToken'];
+      signinWithGoogle.mutate(id);
     } else {
-        const result = await signIn('google'); // 'google' corresponds to the provider name in your NextAuth configuration
+      const result = await signIn('google'); // 'google' corresponds to the provider name in your NextAuth configuration
     }
   };
 
@@ -66,8 +67,10 @@ function LoginPage() {
     }
   }, [query, seType, setAll])
 
+
+
   const signinWithGoogle = useMutation({
-    mutationFn: (data: string) => httpService.get(`${URLS.SIGN_IN_WTIH_CREDENTIALS}`, {
+    mutationFn: (data: string) => httpServiceGoogle.get(`${URLS.SIGN_IN_WTIH_CREDENTIALS}`, {
       headers: {
         Authorization: `Bearer ${data}`,
       }
@@ -79,59 +82,76 @@ function LoginPage() {
         title: 'Success',
         description: 'Signin Successful',
         status: 'success',
+        position: 'top-right'
       })
-      if(!data?.data?.firstName){ 
+      if (!data?.data?.firstName) {
         setShowModal(true)
-      }  
+      } else {
+        localStorage.setItem('token', data?.data?.access_token);
+        localStorage.setItem('refresh_token', data?.data?.refresh_token);
+        localStorage.setItem('user_id', data?.data?.user_id);
+        localStorage.setItem('expires_in', data?.data?.expires_in);
+        setAll({
+          firstName: data?.data?.firstName,
+          lastName: data?.data?.firstName,
+          username: data?.data?.user_name,
+          userId: data?.data?.user_id,
+        })
+        if (type === "EVENT") {
+          router.push(`/dashboard/event/details/${typeID}`);
+        } else {
+          router.push(`/share?type=${type}&typeID=${typeID}`);
+        }
+      }
       setCheckData(data?.data)
     },
     onError: (error: any) => {
       console.log(error);
       toast({
         title: 'Erroor',
-        description: error,
+        description: 'An error occured, please try again',
         status: 'error',
       })
     }
-  })
+  }) 
 
   React.useEffect(() => {
-    if(checkData?.user_id){
-      if(!checkData?.firstName){
+    if (checkData?.user_id) {
+      if (!checkData?.firstName) {
         setShowModal(true)
-      } else { 
+      } else {
         localStorage.setItem('token', checkData.access_token);
         localStorage.setItem('refresh_token', checkData.refresh_token);
         localStorage.setItem('user_id', checkData.user_id);
         localStorage.setItem('expires_in', checkData.expires_in);
-      setAll({
-        firstName: checkData.firstName,
-        lastName: checkData.firstName,
-        username: checkData.user_name,
-        userId: checkData.user_id,
-      })
-      router.push('/share');
+        setAll({
+          firstName: checkData.firstName,
+          lastName: checkData.firstName,
+          username: checkData.user_name,
+          userId: checkData.user_id,
+        })
+        router.push('/share');
       }
     }
   }, [checkData, router, setAll]);
 
-  const checkUserName = useQuery(['username'+UserName], () => httpService.get('/auth/username-check?username='+UserName), {
+  const checkUserName = useQuery(['username' + UserName], () => httpService.get('/auth/username-check?username=' + UserName), {
     onError: (error: any) => {
       toast({
         title: 'Error',
         description: error.response?.data,
         status: 'error',
       })
-    }, 
-    onSuccess: (data) => { 
-      console.log(data?.data?.message); 
-      if(data?.data?.message === "Username already exists."){
+    },
+    onSuccess: (data) => {
+      console.log(data?.data?.message);
+      if (data?.data?.message === "Username already exists.") {
         setCheckUsername(data?.data?.message)
       } else {
         setCheckUsername("")
       }
     }
-  })  
+  })
 
   const { mutate, isLoading } = useMutation({
     mutationFn: (data) => httpService.post(`${URLS.LOGIN}`, data),
@@ -166,77 +186,77 @@ function LoginPage() {
       })
       const typee = sessionStorage.getItem('type');
       const typeIDD = sessionStorage.getItem('typeID');
-      if(type === "EVENT"){
+      if (type === "EVENT") {
         router.push(`/dashboard/event/details/${typeID}`);
-      }   else { 
+      } else {
         router.push(`/share?type=${type}&typeID=${typeID}`);
       }
     }
   });
 
-  const clickHandler =async()=> {
-    setLoading(true); 
-    if(!FirstName){
+  const clickHandler = async () => {
+    setLoading(true);
+    if (!FirstName) {
       toast({
         title: 'Erroor',
         description: 'Enter firstname',
         status: 'error',
       })
-    } else if(!LastName) {
+    } else if (!LastName) {
       toast({
         title: 'Erroor',
         description: 'Enter lastname',
         status: 'error',
       })
-    } else if(!UserName) {
+    } else if (!UserName) {
       toast({
         title: 'Erroor',
         description: 'Enter username',
         status: 'error',
       })
-    } else {  
-        const response = await httpService.put(`/user/update-profile`, {
-          firstName: FirstName,
-          lastName: LastName,
-          username: UserName,
-        }) 
-        if (response) { 
-          toast({
-            title: 'Success',
-            description: 'Update successful',
-            status: 'success',
-          })
-          localStorage.setItem('firstName', FirstName);
+    } else {
+      const response = await httpService.put(`/user/update-profile`, {
+        firstName: FirstName,
+        lastName: LastName,
+        username: UserName,
+      })
+      if (response) {
+        toast({
+          title: 'Success',
+          description: 'Update successful',
+          status: 'success',
+        })
+        localStorage.setItem('firstName', FirstName);
 
-          let newObj = {...checkData, firstName: FirstName}
+        let newObj = { ...checkData, firstName: FirstName }
 
-          localStorage.setItem('token', checkData.access_token);
-          localStorage.setItem('refresh_token', checkData.refresh_token);
-          localStorage.setItem('user_id', checkData.user_id);
-          localStorage.setItem('expires_in', checkData.expires_in);
-          setAll({
-            firstName: checkData.firstName,
-            lastName: checkData.firstName,
-            username: checkData.user_name,
-            userId: checkData.user_id,
-          })
-          if(type === "EVENT"){
-            router.push(`/dashboard/event/details/${typeID}`);
-          }   else { 
-            router.push(`/share?type=${type}&typeID=${typeID}`);
-          }
-          // navigate("/explore")
+        localStorage.setItem('token', checkData.access_token);
+        localStorage.setItem('refresh_token', checkData.refresh_token);
+        localStorage.setItem('user_id', checkData.user_id);
+        localStorage.setItem('expires_in', checkData.expires_in);
+        setAll({
+          firstName: checkData.firstName,
+          lastName: checkData.firstName,
+          username: checkData.user_name,
+          userId: checkData.user_id,
+        })
+        if (type === "EVENT") {
+          router.push(`/dashboard/event/details/${typeID}`);
         } else {
-          toast({
-            title: 'Erroor',
-            description: 'Something wetn wrong',
-            status: 'error',
-          })
-        } 
+          router.push(`/share?type=${type}&typeID=${typeID}`);
+        }
+        // navigate("/explore")
+      } else {
+        toast({
+          title: 'Erroor',
+          description: 'Something wetn wrong',
+          status: 'error',
+        })
+      }
 
     }
     setLoading(false);
-  } 
+  }
 
   const { renderForm } = useForm({
     defaultValues: {
@@ -247,7 +267,7 @@ function LoginPage() {
     submit: (data) => mutate(data)
   });
   return renderForm(
-    <VStack width='100%' height='100vh' overflow={'hidden'} alignItems={'flex-start'} justifyContent={['flex-start','center']} bg='white' paddingX={['0px', '150px',]} spacing={0} position={'relative'}>
+    <VStack width='100%' height='100vh' overflow={'hidden'} alignItems={'flex-start'} justifyContent={['flex-start', 'center']} bg='white' paddingX={['0px', '150px',]} spacing={0} position={'relative'}>
 
 
       <Box width={'100%'} height={'100%'} flex='1' overflowX={'hidden'} overflowY={'auto'} padding={['10px', '0px']}>
@@ -278,7 +298,7 @@ function LoginPage() {
             {/* RIGHT SECTION */}
             <VStack flex='0.5' paddingBottom={['30px', '0px']} width={['100%', '50%']} marginTop={['30px', '0px']} height={['100%']} justifyContent={'center'} alignItems={'center'} spacing={6}>
 
-              <VStack width={['100%','463px']} height='374px' borderWidth={'0.2px'} borderRadius={'24px 0px 24px 24px'} borderColor={'grey'} paddingTop={'30px'} paddingX='20px'>
+              <VStack width={['100%', '463px']} height='374px' borderWidth={'0.2px'} borderRadius={'24px 0px 24px 24px'} borderColor={'grey'} paddingTop={'30px'} paddingX='20px'>
                 <CustomText fontSize={'44px'} fontFamily={'DM-Bold'} fontWeight={'700'} color='#163AB7'>Chasescroll</CustomText>
 
                 <CustomInput name='username' isPassword={false} type='text' placeholder='Enter your username' />
@@ -303,7 +323,7 @@ function LoginPage() {
 
               </VStack>
 
-              <Button onClick={handleSignIn} width={['100%', '294px']}  height={'40px'} borderRadius={'8px'} bg='#1018280D' padding='8px 16px 8px 16px'>
+              <Button onClick={handleSignIn} width={['100%', '294px']} height={'40px'} borderRadius={'8px'} bg='#1018280D' padding='8px 16px 8px 16px'>
                 <Image alt='google' src='/assets/svg/googlelogo.svg' />
                 <CustomText marginLeft={'20px'} fontFamily={'DM-Medium'} fontSize={'16px'} color='black' fontWeight={'700'}>Sign in with Google</CustomText>
               </Button>
@@ -322,13 +342,13 @@ function LoginPage() {
         {LINK2.map((item, index) => {
           if (item.isExternal) {
             return (
-              <CustomText fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} display={{ sm: exclude.includes(item.name) ? 'none':'inline', lg: 'inline'}} key={index.toString()}>
+              <CustomText fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} display={{ sm: exclude.includes(item.name) ? 'none' : 'inline', lg: 'inline' }} key={index.toString()}>
                 <a key={index.toString()} href={item.link}>{item.name}</a>
               </CustomText>
             )
           } else {
             return (
-              <CustomText display={{ sm: exclude.includes(item.name) ? 'none':'inline', lg: 'inline-block'}} fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} key={index.toString()}>
+              <CustomText display={{ sm: exclude.includes(item.name) ? 'none' : 'inline', lg: 'inline-block' }} fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} key={index.toString()}>
                 <Link href={`/${item.link}`} key={index.toString()}>
                   {item.name}
                 </Link>
@@ -342,13 +362,13 @@ function LoginPage() {
         {LINK2.filter((item) => !exclude.includes(item.name)).map((item, index) => {
           if (item.isExternal) {
             return (
-              <CustomText fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} display={{ sm: exclude.includes(item.name) ? 'none':'inline', lg: 'inline'}} key={index.toString()}>
+              <CustomText fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} display={{ sm: exclude.includes(item.name) ? 'none' : 'inline', lg: 'inline' }} key={index.toString()}>
                 <a key={index.toString()} href={item.link}>{item.name}</a>
               </CustomText>
             )
           } else {
             return (
-              <CustomText display={{ sm: exclude.includes(item.name) ? 'none':'inline', lg: 'inline-block'}} fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} key={index.toString()}>
+              <CustomText display={{ sm: exclude.includes(item.name) ? 'none' : 'inline', lg: 'inline-block' }} fontFamily={'DM-Regular'} fontSize={'16px'} marginX={'10px'} key={index.toString()}>
                 <Link href={`/${item.link}`} key={index.toString()}>
                   {item.name}
                 </Link>
@@ -366,20 +386,20 @@ function LoginPage() {
                 <p className=" font-semibold text-xl " >Add User Information To Continue</p>
                 <div className=" w-full flex flex-col gap-2 mt-6 " >
                   <p>First Name</p>
-                  <Input onChange={(e)=> setFirstName(e?.target?.value)} />
+                  <Input onChange={(e) => setFirstName(e?.target?.value)} />
                 </div>
                 <div className=" w-full flex flex-col gap-2  " >
                   <p>Last Name</p>
-                  <Input onChange={(e)=> setLastName(e?.target?.value)} />
+                  <Input onChange={(e) => setLastName(e?.target?.value)} />
                 </div>
                 <div className=" w-full flex flex-col gap-2  " >
                   <p>Username</p>
-                  <Input onChange={(e)=> setUserName(e?.target?.value)} />
-                  {CheckUsername && 
+                  <Input onChange={(e) => setUserName(e?.target?.value)} />
+                  {CheckUsername &&
                     <p className=" text-sm text-chasescrollRed -mt-1 " >{CheckUsername}</p>
                   }
                 </div>
-                <button disabled={(!FirstName || !LastName || UserName?.length < 3 || CheckUsername) ? true : false} onClick={()=> clickHandler()} className=" w-full h-[45px] bg-chasescrollBlue text-white disabled:opacity-25 rounded-md mt-6 font-semibold " >{Loading ? "Loading..." : "Update Information"}</button>
+                <button disabled={(!FirstName || !LastName || UserName?.length < 3 || CheckUsername) ? true : false} onClick={() => clickHandler()} className=" w-full h-[45px] bg-chasescrollBlue text-white disabled:opacity-25 rounded-md mt-6 font-semibold " >{Loading ? "Loading..." : "Update Information"}</button>
               </div>
             </div>
 
