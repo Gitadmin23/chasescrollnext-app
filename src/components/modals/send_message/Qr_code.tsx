b18c79b5c6b73878bd18e3d6b4bb31bb987ca1e7
egileekada <egileoniso.ekada@gmail.com>
2023-12-07 17:34:18 +0100

    fixing deployment error

--- src/components/modals/send_message/Qr_code.tsx
+++ src/components/modals/send_message/Qr_code.tsx
@@ -8,9 +8,7 @@ import { IoClose } from 'react-icons/io5';
 import QRCode from "react-qr-code";
 import { useReactToPrint } from 'react-to-print';
 import {
-    exportComponentAsJPEG,
-    exportComponentAsPDF,
-    exportComponentAsPNG
+    exportComponentAsJPEG, 
 } from "react-component-export-image";
 
 interface Props {
@@ -26,42 +24,7 @@ function Qr_code(props: Props) {
         data
     } = props
 
-    const componentRef: any = React.useRef();
-    // const handlePrint = useReactToPrint({
-    //     content: () => componentRef.current
-    // });
-
-    // "linear-gradient(0deg, rgba(18, 18, 18, 0.10) 0%, rgba(18, 18, 18, 0.10) 100%), linear-gradient(98deg, rgba(93, 112, 249, 0.39) -16.16%, #1732F7 125.87%)"
-
-
-    const ComponentToPrint = React.forwardRef((props, ref: any): any => (
-        <Flex ref={ref} alignItems={"center"} >
-            <Flex width={"400px"} mx={"auto"} flexDir={"column"} alignItems={"center"} position={"relative"} roundedTop={"6px"} >
-                <Flex pt={"4"} >
-                    <HStack zIndex={20} justifyContent={'center'}>
-                        <Image src='/assets/images/chasescroll-logo.png' width={30} height={30} alt='logo' />
-                        <CustomText fontFamily={'Satoshi-Regular'} color='#FFF'>Chasescroll</CustomText>
-                    </HStack>
-                </Flex>
-                <Flex justifyContent={"center"} roundedTop={"6px"} pt={"4"} color={"white"} zIndex={20} width={"full"} >
-                    <Text zIndex={20} fontWeight={"semibold"} >Event Name: {data?.eventName}</Text>
-                </Flex>
-                <Box position={"absolute"} left={"0px"} right={"0px"} height={"300px"} roundedTop={"6px"} roundedBottom={"full"} zIndex={10} top={"0px"} style={{ background: "#5D70F9" }} />
-                <Flex justifyContent={"center"} flex={1} width={"full"} pt={"6"} >
-                    <Box width={"60%"} shadow={"lg"} zIndex={20} bg={"white"} p={"3"} rounded={"md"} >
-                        <QRCode
-                            style={{ height: "auto", maxWidth: "100%", width: "100%", zIndex: 20 }}
-                            value={`${WEBSITE_URL}/event/${id}`}
-                            viewBox={`0 0 256 256`}
-                        />
-                    </Box>
-                </Flex>
-                <Text my={"4"} fontWeight={"semibold"} color={"#121212CC"} >Scan here and get Your Event Link</Text>
-            </Flex>
-        </Flex>
-    ));
-
-
+    const componentRef: any = React.useRef(); 
 
     return (
         <Flex flexDir={"column"} roundedTop={"6px"} alignItems={"center"} pb={"8"} >
@@ -93,39 +56,7 @@ function Qr_code(props: Props) {
                     </Flex>
                     <Text my={"4"} color={"#121212CC"} >Scan here and get Your Event Link</Text>
                 </Flex>
-            </Flex>
-            {/* {print && (  */}
-            {/* <Box display={"none"} > */}
-
-            {/* <Flex ref={componentRef} alignItems={"center"} >
-                    <Flex width={"400px"} mx={"auto"} flexDir={"column"} alignItems={"center"} roundedTop={"6px"} >
-                        <Flex pt={"4"} >
-                            <HStack zIndex={20} justifyContent={'center'}>
-                                <Image src='/assets/images/chasescroll-logo.png' width={30} height={30} alt='logo' />
-                                <CustomText fontFamily={'Satoshi-Regular'} color='#FFF'>Chasescroll</CustomText>
-                            </HStack>
-                        </Flex>
-                        <Flex justifyContent={"center"} roundedTop={"6px"} pt={"4"} color={"white"} zIndex={20} width={"full"} >
-                            <Text zIndex={20} fontWeight={"semibold"} >Event Name: {data?.eventName}</Text>
-                        </Flex>
-                        <Box position={"absolute"} left={"0px"} right={"0px"} height={"300px"} roundedTop={"6px"} roundedBottom={"full"} zIndex={10} top={"0px"} style={{ background: "#5D70F9" }} />
-                        <Flex justifyContent={"center"} flex={1} width={"full"} pt={"6"} >
-                            <Box width={"60%"} shadow={"lg"} zIndex={20} bg={"white"} p={"3"} rounded={"md"} >
-                                <QRCode
-                                    style={{ height: "auto", maxWidth: "100%", width: "100%", zIndex: 20 }}
-                                    value={`${WEBSITE_URL}/event/${id}`}
-                                    viewBox={`0 0 256 256`}
-                                />
-                            </Box>
-                        </Flex>
-                        <Text my={"4"} fontWeight={"semibold"} color={"#121212CC"} >Scan here and get Your Event Link</Text>
-                    </Flex>
-                </Flex> */}
-            {/* <ComponentToPrint ref={componentRef} /> */}
-            {/* </Box> */}
-
-            {/* )} */}
-            {/* <Text my={"4"} color={"#121212CC"} >Scan here and get Your Event Link</Text> */}
+            </Flex> 
             <CustomButton maxWidth={"300px"} backgroundColor={"#8893E4"} onClick={() => exportComponentAsJPEG(componentRef)} text='Download QR-Code' />
         </Flex>
     )
