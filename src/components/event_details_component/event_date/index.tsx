50ab3eb5829f5143b38caf3567c9b72f84b972de
egileekada <egileoniso.ekada@gmail.com>
2023-12-11 16:52:20 +0100

    update on time zone

--- src/components/event_details_component/event_date/index.tsx
+++ src/components/event_details_component/event_date/index.tsx
@@ -1,6 +1,7 @@
 import { BlueCalendarIcon } from '@/components/svg'
 import { dateFormat, timeFormat } from '@/utils/dateFormat'
 import { Box, Flex, Text } from '@chakra-ui/react'
+import moment from 'moment'
 import React from 'react'
 
 interface Props {
@@ -12,7 +13,7 @@ function EventDate(props: Props) {
     const {
         date,
         name
-    } = props
+    } = props 
 
     return (
         <Box display={"flex"} flexDirection={"column"} borderBottomWidth={"1px"} roundedBottom={"lg"} px={"2"} alignItems={"start"} pb={"2"} >
@@ -21,7 +22,7 @@ function EventDate(props: Props) {
                 <BlueCalendarIcon />
                 <Box>
                     <Text fontWeight={"bold"} color={"brand.chasescrollBlue"} >{dateFormat(date)}</Text>
-                    <Text fontWeight={"semibold"} fontSize={"sm"} color={"brand.chasescrollTextGrey2"} >{timeFormat(date)} (GMT)</Text>
+                    <Text fontWeight={"semibold"} fontSize={"sm"} color={"brand.chasescrollTextGrey2"} >{timeFormat(date)} ({new Date(date).toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2]})</Text>
                 </Box>
             </Flex>
         </Box>
