3a68b306813253c3ad2578a3944f8e899ce4d49b
daniel <danielemmanuel257@gmail.com>
2023-12-23 15:21:00 +0100

    changed keys

--- src/hooks/awsHook.ts
+++ src/hooks/awsHook.ts
@@ -4,8 +4,8 @@ import { Upload } from "@aws-sdk/lib-storage";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const cred = {
-    accessKeyId: 'AKIA6I6LF3LQ4YD4GL5H',
-    secretAccessKey: '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'
+    accessKeyId: 'AKIA6I6LF3LQTCYBFYNR',
+    secretAccessKey: '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'
 }
 
 const AWSHook = () => {
