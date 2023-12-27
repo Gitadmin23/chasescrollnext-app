209b08d29f1616ff1d9e8028e97ba8500c5e4c51
daniel <danielemmanuel257@gmail.com>
2023-12-27 23:35:28 +0100

    changed variables

--- src/hooks/awsHook.ts
+++ src/hooks/awsHook.ts
@@ -4,8 +4,8 @@ import { Upload } from "@aws-sdk/lib-storage";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const cred = {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_SECRET_KEY as string,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY as string
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY as string
 }
 
 const AWSHook = () => {
