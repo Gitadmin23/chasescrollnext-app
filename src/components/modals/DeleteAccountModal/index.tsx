88c8b74420435c5dc15b96fa2e6c9baecc6077a0
egileekada <egileoniso.ekada@gmail.com>
2024-01-19 14:39:33 +0100

    fixing deleting of account issuse

--- src/components/modals/DeleteAccountModal/index.tsx
+++ src/components/modals/DeleteAccountModal/index.tsx
@@ -5,6 +5,7 @@ import { Modal, ModalBody, ModalContent, ModalOverlay, VStack, Image, Button, us
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { useMutation } from 'react-query';
+import { useSession , signOut } from 'next-auth/react'
 
 type Props = {
     isOpen: boolean;
@@ -26,10 +27,8 @@ function DeleteAccoutModal({ isOpen, onClose }: Props) {
                 duration: 3000,
                 isClosable: true,
             });
-            localStorage.clear();
-            sessionStorage.clear();
-            router.replace('/auth');
-        }, 
+            clearData()
+        },
         onError: () => {
             toast({
                 title: 'Error',
@@ -41,24 +40,31 @@ function DeleteAccoutModal({ isOpen, onClose }: Props) {
             });
         },
     });
-    
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent width='350px' bg='white' height='326px' borderRadius={'16px'}>
-            <ModalBody >
-                <VStack width='100%' justifyContent={'center'} height='100%' alignItems={'center'} spacing={3}>
-                    <Image alt='delete' src='/assets/images/deleteaccount.svg' />
-                    <CustomText fontFamily='DM-Bold' textAlign={'center'} fontSize={'20px'}>Delete Account</CustomText>
-                    <CustomText fontFamily={'DM-Regular'} textAlign={'center'} fontSize={'16px'} color='grey'>Are you sure you want  to delete this account ? this action cannot be undone.</CustomText>
 
-                    <Button isLoading={isLoading} onClick={() => mutate()} width='100%' height='42px' bg='red' color="white" variant='solid'>Delete</Button>
-                    <Button onClick={() => onClose()} width='100%' height='42px' borderWidth={'0px'} color="grey" variant='outline' outlineColor={'lightgrey'}>Cancel</Button>
-                </VStack>
-            </ModalBody>
-        </ModalContent>
-    </Modal>
-  )
+    async function clearData() { 
+        localStorage.clear();
+        await signOut();
+        sessionStorage.clear();
+        router.push('/auth');
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+            <ModalOverlay />
+            <ModalContent width='350px' bg='white' height='326px' borderRadius={'16px'}>
+                <ModalBody >
+                    <VStack width='100%' justifyContent={'center'} height='100%' alignItems={'center'} spacing={3}>
+                        <Image alt='delete' src='/assets/images/deleteaccount.svg' />
+                        <CustomText fontFamily='DM-Bold' textAlign={'center'} fontSize={'20px'}>Delete Account</CustomText>
+                        <CustomText fontFamily={'DM-Regular'} textAlign={'center'} fontSize={'16px'} color='grey'>Are you sure you want  to delete this account ? this action cannot be undone.</CustomText>
+
+                        <Button isLoading={isLoading} onClick={() => mutate()} width='100%' height='42px' bg='red' color="white" variant='solid'>Delete</Button>
+                        <Button onClick={() => onClose()} width='100%' height='42px' borderWidth={'0px'} color="grey" variant='outline' outlineColor={'lightgrey'}>Cancel</Button>
+                    </VStack>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    )
 }
 
 export default DeleteAccoutModal
