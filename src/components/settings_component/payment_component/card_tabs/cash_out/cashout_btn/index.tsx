28f3014773a66c6d376f198bc2e7cf1e3b9ff598
egileekada <egileoniso.ekada@gmail.com>
2024-02-15 17:39:33 +0100

    update on withdrawal

--- src/components/settings_component/payment_component/card_tabs/cash_out/cashout_btn/index.tsx
+++ src/components/settings_component/payment_component/card_tabs/cash_out/cashout_btn/index.tsx
@@ -6,6 +6,8 @@ import BankOtp from '../modals/bank_otp'
 import httpService from '@/utils/httpService'
 import CustomButton from '@/components/general/Button'
 import useSettingsStore from '@/global-state/useSettingsState'
+import { useMutation } from 'react-query'
+import { useRouter } from 'next/navigation'
 
 interface Props {
     currency: string,
@@ -20,9 +22,13 @@ function CashoutBtn(props: Props) {
 
     const [open, setOpen] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
+    const [loadingWithdrawal, setLoadingWithdrawal] = React.useState(false)
     const [modalType, setModalType] = React.useState(0)
     const [transferCode, settransferCode] = React.useState("")
+    const [transferRecipient, setTransferRecipient] = React.useState("") 
+    const [accountName, setAccountName] = React.useState("")
     const toast = useToast()
+    const navigate = useRouter()
 
     const { setAmount } = useSettingsStore((state) => state);
 
@@ -32,7 +38,7 @@ function CashoutBtn(props: Props) {
         if (currency === "USD") {
             response = await httpService.get("/payments/account/check")
         } else {
-            response = await httpService.get("/payments/account/checkPaystack")
+            setOpen(true)
         }
         if (!response?.data) {
             if (currency === "USD") {
@@ -45,54 +51,62 @@ function CashoutBtn(props: Props) {
                 setOpen(true)
             }
         } else {
-            const request: any = await httpService.post(`/payments/account/withdraw?currency=${currency}&amount=${amount}`)
-            
-            if (request?.data?.status === "SUCCESS") {
-                toast({
-                    title: 'Success',
-                    description: "Withdraw Success",
-                    status: 'success',
-                    isClosable: true,
-                    duration: 5000,
-                    position: 'top-right',
-                });
-                setAmount("")
-            } else if (request?.data?.status === "ok") {
-                toast({
-                    title: 'Success',
-                    description: "Withdraw Success",
-                    status: 'success',
-                    isClosable: true,
-                    duration: 5000,
-                    position: 'top-right',
-                });
-
-                setAmount("")
-            } else if (request?.data?.status === "OTP") {
-                settransferCode(request?.transfer_code)
-                setModalType(1)
-                setOpen(true)
-            } else {
-
-                toast({
-                    title: 'Error',
-                    description: "Error Occurred",
-                    status: 'error',
-                    isClosable: true,
-                    duration: 5000,
-                    position: 'top-right',
-                }); 
-            }
+            WithdrawFund()
         }
         setLoading(false)
+    }
+
+    const WithdrawFund = async (item?: any)=> {
+        setLoadingWithdrawal(true)
+        const request: any = await httpService.post(`/payments/account/withdraw?currency=${currency}&amount=${amount}&transferRecipient=${item ? item : transferRecipient}`)
+            
+        if (request?.data?.status === "SUCCESS") {
+            toast({
+                title: 'Success',
+                description: "Withdraw Success",
+                status: 'success',
+                isClosable: true,
+                duration: 5000,
+                position: 'top-right',
+            });
+            setAmount("")
+            navigate.refresh()
+        } else if (request?.data?.status === "ok") {
+            toast({
+                title: 'Success',
+                description: "Withdraw Success",
+                status: 'success',
+                isClosable: true,
+                duration: 5000,
+                position: 'top-right',
+            });
+
+            setAmount("")
+            navigate.refresh()
+        } else if (request?.data?.status === "OTP") {
+            settransferCode(request?.transfer_code)
+            setModalType(1)
+            setOpen(true)
+        } else {
+
+            toast({
+                title: 'Error',
+                description: "Error Occurred",
+                status: 'error',
+                isClosable: true,
+                duration: 5000,
+                position: 'top-right',
+            }); 
+        }
+        setLoadingWithdrawal(false)
     }
 
     return (
         <Box width={"full"} >
             <CustomButton backgroundColor={"#12299C"} onClick={() => clickHandler()} text='Withdraw' isLoading={loading} disable={loading || !amount} marginTop={"8"} />
-            <ModalLayout open={open} close={setOpen} title='Bank Information' >
+            <ModalLayout open={open} close={setOpen} title='Recipient' >
                 {modalType === 0 && (
-                    <AddBankInfo close={setOpen} />
+                    <AddBankInfo loading={loadingWithdrawal} withdraw={WithdrawFund} setTransferRecipient={setTransferRecipient} transferRecipient={transferRecipient} setAccountName={setAccountName} accountName={accountName} close={setOpen} />
                 )}
                 {modalType === 1 && (
                     <BankOtp close={setOpen} currency={currency} transferCode={transferCode} />
---
+++ src/components/settings_component/payment_component/card_tabs/cash_out/modals/add_bank_info/account_list/index.tsx
@@ -0,0 +1,73 @@
+import LoadingAnimation from '@/components/sharedComponent/loading_animation';
+import { RoundArrow } from '@/components/svg';
+import httpService from '@/utils/httpService';
+import { Box, Flex, Text, useToast } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { useQuery } from 'react-query';
+
+interface Props {
+    setTransferRecipient: any,
+    withdraw: any
+}
+
+function AccountList(props: Props) {
+    const {
+        setTransferRecipient,
+        withdraw
+    } = props
+
+    const toast = useToast()
+    const [data, setData] = useState([] as any)
+
+    const ref: any = React.useRef(null); 
+
+    const scroll = (scrolloffset : any ) =>{
+        ref.current.scrollLeft += scrolloffset 
+    };        
+    // react query
+    const { isLoading, isRefetching } = useQuery(['get-bank-list'], () => httpService.get("/payments/account/paystackBankAccounts"), {
+        onError: (error: any) => {
+            toast({
+                status: "error",
+                title: error.response?.data,
+            });
+        },
+        onSuccess: (data) => {
+            setData(data?.data?.content);
+
+        }
+    })
+
+    return (
+        <Flex flexDir={"column"} width={"full"} >
+            <Text lineHeight={"13.02px"} fontSize={"10px"} color={"#626262"} >Recent Transaction</Text>
+            <LoadingAnimation loading={isLoading} refeching={isRefetching} length={data?.length} >
+                <Flex ref={ref} w={"full"} overflowX={"auto"} scrollBehavior={"smooth"} pt={"4"} pb={"1"} mb={"2"} >
+                    <Flex w={"auto"} gap={"4"} >
+                        {data?.map((item: any, index: number) => {
+                            return (
+                                <Box onClick={() => withdraw(item?.accountName)} as='button' flexWrap={"nowrap"} >
+                                    <Box position={"relative"} width={"fit-content"} >
+                                        <Text lineHeight={"18.23px"} fontSize={"14px"} fontWeight={"bold"} color={"#121212"} >{item?.accountName}</Text>
+                                        <Text lineHeight={"18.23px"} top={"0px"} mt={"-18.23px"} opacity={"0"} fontSize={"14px"} fontWeight={"bold"} color={"#121212"} >Otuekong_Archibong</Text>
+                                    </Box>
+                                    <Text color={"#B6B6B6"} fontSize={"14px"} fontWeight={"normal"} >{item?.bankName}</Text>
+                                </Box>
+                            )
+                        })} 
+                    </Flex>
+                </Flex>
+            </LoadingAnimation>
+            <Flex gap={"2"} width={"full"} justifyContent={"end"} >
+                <Box as='button' onClick={() => scroll(-200)} transform={"rotate(180deg)"} >
+                    <RoundArrow />
+                </Box>
+                <Box as='button' onClick={() => scroll(200)} >
+                    <RoundArrow />
+                </Box>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default AccountList
