56b5428cc718221a4afa2e780a5e7d52ed5764c7
ekadaegile <egileoniso.ekada@gmail.com>
2024-06-11 16:36:02 +0100

    update

--- src/components/landing_component/landingPageLayout.tsx
+++ src/components/landing_component/landingPageLayout.tsx
@@ -1,13 +1,31 @@
+"use client"
 import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import FooterLandingPage from './footer'
-import HomeNavbar from './navbar'
+import HomeNavbar from './navbar'  
+import { useRouter } from 'next/navigation'
 
 interface IProps {
     children: React.ReactNode
 }
 
 export default function LandingPageLayout({ children }: IProps) {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            if (typeof window !== 'undefined') {
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                // behavior: 'smooth', // You can set this to 'auto' if you prefer instant scrolling
+              });
+            } 
+        }; 
+        handleRouteChange()
+    }, [router]);
+
     return (
         <Flex h={"100vh"} w={"full"} scrollBehavior={"smooth"} overflowY={"auto"} flexDir={"column"} pos={"relative"} >
             <Flex w={"full"} pos={"sticky"} zIndex={"100"} top={"0px"} >
