7e5d67be3451e5ddac8b89a41d7fff6aa669248d
ekadaegile <egileoniso.ekada@gmail.com>
2024-06-13 07:28:00 +0100

    update on top navbar

--- src/components/landing_component/landingPageLayout.tsx
+++ src/components/landing_component/landingPageLayout.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Flex } from '@chakra-ui/react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import FooterLandingPage from './footer'
 import HomeNavbar from './navbar'
 import { useRouter } from 'next/navigation'
@@ -12,6 +12,22 @@ interface IProps {
 export default function LandingPageLayout({ children }: IProps) {
 
     const router = useRouter();
+    const [isAtTop, setIsAtTop] = useState(true);
+  
+    const checkScrollTop = () => {
+      if (window.scrollY === 0) {
+        setIsAtTop(true);
+      } else {
+        setIsAtTop(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', checkScrollTop);
+      return () => {
+        window.removeEventListener('scroll', checkScrollTop);
+      };
+    }, []);
 
     useEffect(() => {
         const handleRouteChange = () => {
@@ -28,7 +44,7 @@ export default function LandingPageLayout({ children }: IProps) {
 
     return (
         <Flex h={"100vh"} w={"full"} overflowX={"hidden"} scrollBehavior={"smooth"} overflowY={"auto"} flexDir={"column"} pos={"relative"} >
-            <Flex w={"full"} pos={"sticky"} zIndex={"100"} top={"0px"} >
+            <Flex w={"full"} pos={isAtTop ? "relative" : "sticky"} zIndex={"100"} top={"0px"} >
                 <HomeNavbar />
             </Flex>
             {children}
