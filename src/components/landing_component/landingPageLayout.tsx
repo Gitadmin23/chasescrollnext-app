fa5af9b6a0fa83e29512bfbfe9626127f46aa26e
ekadaegile <egileoniso.ekada@gmail.com>
2024-06-14 00:17:14 +0100

    update on top bar error

--- src/components/landing_component/landingPageLayout.tsx
+++ src/components/landing_component/landingPageLayout.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import React, { useEffect, useRef, useState } from 'react'
 import FooterLandingPage from './footer'
 import HomeNavbar from './navbar'
@@ -11,30 +11,22 @@ interface IProps {
 
 export default function LandingPageLayout({ children }: IProps) {
 
-    const router = useRouter(); 
-
-    const contentRef: any = useRef(null);
-
-    const scrollToTop = () => {
-        if (contentRef.current) {
-            contentRef.current.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        }
-    };
+    const router = useRouter();
 
     useEffect(() => {
-        scrollToTop()
-    }, [router])
+        window.scrollTo(0, 0)
+      }, [router])
 
     return (
-        <Flex ref={contentRef} minH={"100vh"} w={"full"} overflowX={"hidden"} scrollBehavior={"smooth"} overflowY={"auto"} flexDir={"column"} pos={"relative"} >
-            <Flex w={"full"} pos={"sticky"} zIndex={"100"} top={"0px"} >
+        <Flex h={"100vh"} w={"full"} overflow={"hidden"} scrollBehavior={"smooth"} flexDir={"column"} pos={"relative"} >
+            <Flex w={"full"} position={"static"} top={"0px"} insetX={"0px"} zIndex={"100"} >
                 <HomeNavbar />
             </Flex>
-            {children}
-            <FooterLandingPage />
+            <Flex h={"100vh"} w={"full"} overflowY={"auto"} overflowX={"hidden"} scrollBehavior={"smooth"} flexDir={"column"} >
+                {/* <Box w={"full"} height={["64px", "64px", "101.03px"]} /> */}
+                {children}
+                <FooterLandingPage />
+            </Flex>
         </Flex>
     )
 }
