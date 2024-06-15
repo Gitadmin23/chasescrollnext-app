9d84d7a69f831a4b3b0849dcfea339b69ebe8b83
ekadaegile <egileoniso.ekada@gmail.com>
2024-06-15 08:27:17 +0100

    update on view ticket button on my event

--- src/components/event_details_component/get_event_ticket/index.tsx
+++ src/components/event_details_component/get_event_ticket/index.tsx
@@ -136,17 +136,21 @@ function GetEventTicket(props: Props) {
     });
   }, [createTicket]);
 
-    const goback = (item: boolean) => {
+    const goback = () => {
+        if (showModal) {
+            setShowModal(false)
+        } else {
+            setShowModal(true)
+        }
         setModalTab(4)
-        setShowModal(item)
     }
 
     return (
-        <> 
+        <>
             {!carousel && (
-                <> 
+                <>
                     {!selectedTicket?.rerouteURL ?
-                        <CustomButton backgroundColor={isBought ? "#3EC259": ""} opacity={(!selectedTicket?.ticketType && !isBought) ? "30%" : ""} my={"auto"} onClick={clickHandler} disable={(!selectedTicket?.ticketType || selectedTicket?.ticketType || isBought) ? false : true} text={((isBought) ? "View" : isFree ? "Register" : "Buy") + " Ticket"} width={["full", "400px", "400px", "full"]} /> :
+                        <CustomButton backgroundColor={isBought ? "#3EC259" : ""} opacity={(!selectedTicket?.ticketType && !isBought) ? "30%" : ""} my={"auto"} onClick={clickHandler} disable={(!selectedTicket?.ticketType || selectedTicket?.ticketType || isBought) ? false : true} text={((isBought) ? "View" : isFree ? "Register" : "Buy") + " Ticket"} width={["full", "400px", "400px", "full"]} /> :
                         <a href={selectedTicket?.rerouteURL} target="_blank" >
                             <CustomButton backgroundColor={isBought ? "#3EC259" : ""} opacity={(!selectedTicket?.ticketType && !isBought) ? "30%" : ""} my={"auto"} onClick={clickHandler} disable={(!selectedTicket?.ticketType || selectedTicket?.ticketType || isBought) ? false : true} text={((isBought) ? "View" : isFree ? "Register" : "Buy") + " Ticket"} width={["full", "400px", "400px", "full"]} />
                         </a>
