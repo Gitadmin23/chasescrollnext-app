77f8660715990a52a3a46c1068c313df5f181351
ekadaegile <egileoniso.ekada@gmail.com>
2024-06-27 19:25:03 +0100

    update on event date

--- src/components/create_event_component/event_information/select_date/index.tsx
+++ src/components/create_event_component/event_information/select_date/index.tsx
@@ -1,7 +1,7 @@
 import { CalendarIcon } from '@/components/svg'
 import useEventStore from '@/global-state/useCreateEventState';
 import { dateFormat, timeFormat } from '@/utils/dateFormat';
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, useToast } from '@chakra-ui/react'
 import React, { forwardRef } from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -18,6 +18,7 @@ function SelectDate(props: Props) {
     } = props
 
     const { eventdata, updateEvent } = useEventStore((state) => state);
+    const toast = useToast()
 
     const handleDateSelect = (date: any) => {
         if (name === "Start") {
@@ -27,11 +28,35 @@ function SelectDate(props: Props) {
                 startTime: Date.parse(new Date(date).toJSON())
             })
         } else {
-            updateEvent({
-                ...eventdata,
-                endDate: Date.parse(new Date(date).toJSON()),
-                endTime: Date.parse(new Date(date).toJSON())
-            })
+            if (!eventdata.startDate) {
+                toast({
+                    title: 'Error',
+                    description: "Please enter your starting date",
+                    status: 'error',
+                    isClosable: true,
+                    duration: 2000,
+                    position: 'top-right',
+                });
+            } else if (date <= new Date(eventdata?.startDate)) {
+                toast({
+                    title: 'Error',
+                    description: "Please valid end date",
+                    status: 'error',
+                    isClosable: true,
+                    duration: 2000,
+                    position: 'top-right',
+                });
+
+                console.log();
+                console.log(date);
+
+            } else {
+                updateEvent({
+                    ...eventdata,
+                    endDate: Date.parse(new Date(date).toJSON()),
+                    endTime: Date.parse(new Date(date).toJSON())
+                })
+            }
         }
     }
 
@@ -46,6 +71,19 @@ function SelectDate(props: Props) {
         )
     }
 
+    // const clickHandler = () => {
+    //     if (name === "End" && !eventdata.startDate ) {
+    //         toast({
+    //             title: 'Error',
+    //             description: "Please enter your starting date",
+    //             status: 'error',
+    //             isClosable: true,
+    //             duration: 5002,
+    //             position: 'top-right',
+    //         });
+    //     }
+    // }
+
     return (
         <Flex width={"full"} flexDirection={"column"} gap={"2"} py={"2"} >
             <Text fontSize={"sm"} >
@@ -53,10 +91,12 @@ function SelectDate(props: Props) {
             </Text>
             <DatePicker
                 id={name}
-                selected={name === "End" ? new Date(eventdata.startDate) : data ? new Date(data) : new Date()}
+                // value={}
+                // disabled={name === "End" && !eventdata.startDate}
+                selected={name === "End" ? eventdata.startDate ? new Date(data ?? eventdata.startDate) : new Date() : data ? new Date(data) : new Date()}
                 dateFormat="MMM d, yyyy h:mm aa"
                 showTimeSelect
-                minDate={name === "End" ? new Date(eventdata.startDate) : new Date()}
+                minDate={(name === "End") ? (eventdata.startDate ? new Date(eventdata.startDate): new Date()): new Date()}
                 onChange={handleDateSelect}
                 customInput={<CustomInput />}
             />
