0d56c2c3df2ab33f523decb8e18fd57623b51d2f
ekadaegile <egileoniso.ekada@gmail.com>
2024-08-10 23:22:57 +0100

    update

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import SignupModal from '../component/signupModal'
 import { useRouter } from 'next/navigation'
 import GoogleBtn from '@/components/sharedComponent/googlebtn'
+import { IoIosArrowBack } from 'react-icons/io'
 
 export default function Sigup() {
 
@@ -13,8 +14,13 @@ export default function Sigup() {
     return (
         <Flex w={"full"} height={"100vh"} >
             <Flex w={"full"} h={"full"} display={["none", "none", "none", "flex"]}  justifyContent={"center"} alignItems={"center"} position={"relative"} >
-                <Image alt='bg' src='/images/loginimg.jpgpdate' objectPosition={"left"} pos={"absolute"} inset={"0px"} h={"full"} objectFit={"cover"} />
-                <Image  alt='bg' src='/images/bay.png' pos={"relative"} w={"70%"} objectFit={"contain"} />
+                <Image alt='bg' src='/images/loginimg.jpg' objectPosition={"left"} pos={"absolute"} inset={"0px"} h={"full"} objectFit={"cover"} />
+                <Image alt='bg' src='/images/bay.png' pos={"relative"} zIndex={"10"} w={"70%"} objectFit={"contain"} />
+                <Box pos={"absolute"} inset={"0px"} w={"full"} h={"full"} bg={"black"} opacity={"40%"} />
+                <Flex as={"button"} onClick={()=> router?.push("/home")} pos={"absolute"} top={"12"} color={"white"} fontSize={"16px"} fontWeight={"600"} zIndex={"5"} gap={"1"} alignItems={"center"} left={"12"} >
+                    <IoIosArrowBack size={"20px"} />
+                    Home
+                </Flex>
             </Flex>
             <Flex justifyContent={"center"} alignItems={"center"} bgColor={"#FCFCFC"} h={"full"} w={"full"} >
                 <Flex style={{boxShadow: "0px 2px 8px 2px #00000003"}} flexDir={"column"} gap={"1"} justifyContent={"center"} alignItems={"center"} maxW={"500px"} p={"8"} rounded={"62px"} w={"full"} >
