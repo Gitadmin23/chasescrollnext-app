31eb1711362e85d7ccd7cd077d1dd9232e3a5827
egilechase <ejileonisofien.ekada@chasescroll.com>
2024-11-08 14:56:41 +0100

    update on community

--- src/app/dashboard/profile/[slug]/community/page.tsx
+++ src/app/dashboard/profile/[slug]/community/page.tsx
@@ -3,13 +3,13 @@ import CommunityCard from '@/components/search_component/other_components/commun
 import LoadingAnimation from '@/components/sharedComponent/loading_animation';
 import InfiniteScrollerComponent from '@/hooks/infiniteScrollerComponent';
 import { URLS } from '@/services/urls';
-import { Box, Grid, HStack } from '@chakra-ui/react'
+import { Box, Grid, HStack } from '@chakra-ui/react' 
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 function Community({ params }: { params: { slug: string } }) {
 
-
-    // const { search, setSearchValue } = useSearchStore((state) => state); 
+    const router = useRouter()
 
     const { results, isLoading, ref, isRefetching } = InfiniteScrollerComponent({ url: URLS.GET_JOINED_GROUPS + "?userID=" + params?.slug, limit: 10, filter: "id" })
 
@@ -22,13 +22,13 @@ function Community({ params }: { params: { slug: string } }) {
                     {results?.map((community: any, i: number) => {
                         if (results?.length === i + 1) {
                             return (
-                                <Box ref={ref} key={i} width={"full"} borderBottomWidth={"1px"} rounded={"3xl"} px={"4"} pb={"2"}>
+                                <Box ref={ref} key={i} onClick={()=> router?.push(`/dashboard/community?activeID=${community?.id}`)} width={"full"} borderBottomWidth={"1px"} rounded={"3xl"} px={"4"} pb={"2"}>
                                     <CommunityCard profile={true} data={community} />
                                 </Box>
                             )
                         } else {
                             return (
-                                <Box key={i} width={"full"} borderBottomWidth={"1px"} rounded={"3xl"} px={"4"} pb={"2"}>
+                                <Box key={i} onClick={()=> router?.push(`/dashboard/community?activeID=${community?.id}`)} width={"full"} borderBottomWidth={"1px"} rounded={"3xl"} px={"4"} pb={"2"}>
                                     <CommunityCard profile={true} data={community} />
                                 </Box>
                             )
