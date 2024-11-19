df869a7ee90df4e4dd4e1684ea840e07f6a59e53
egilechase <139143075+egilechase@users.noreply.github.com>
2024-11-19 11:46:16 +0100

    Update formatNumberWithK.ts

--- src/utils/formatNumberWithK.ts
+++ src/utils/formatNumberWithK.ts
@@ -1,10 +1,18 @@
 
 export const formatNumberWithK = (number: any) =>{
-    if(number === 0 || !number) {
-        return "0"
-    } else {
-        return number > 999 ? `${Math.trunc(number / 1000)}k` : number
+    // if(number === 0 || !number) {
+    //     return "0"
+    // } else {
+    //     return number > 999 ? `${Math.trunc(number / 1000)}k` : number
+    // }
+     
+    if (num >= 1_000_000) {
+        return (num / 1_000_000).toFixed(1).replace(/\.0$/, "") + "m";
     }
+    if (num >= 1_000) {
+        return (num / 1_000).toFixed(1).replace(/\.0$/, "") + "k";
+    }
+    return num.toString();
 }
 
 export const numberFormatNaire = (x: any) => { 
