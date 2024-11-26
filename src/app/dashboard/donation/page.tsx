0c10d8c2cbdbb2db64ecba768c27f79360ff58e6
egilechase <ejileonisofien.ekada@chasescroll.com>
2024-11-26 16:33:09 +0100

    update on fundraising search

--- src/app/dashboard/donation/page.tsx
+++ src/app/dashboard/donation/page.tsx
@@ -1,38 +1,58 @@
-"use client" 
+"use client"
 import DonationItemList from '@/components/donation/donationItemList'
 import CustomButton from '@/components/general/Button'
-import Fundpaystack from '@/components/settings_component/payment_component/card_tabs/fund_wallet/fundpaystack' 
-import usePaystackStore from '@/global-state/usePaystack' 
-import useCustomTheme from '@/hooks/useTheme' 
-import { Flex, Text } from '@chakra-ui/react'  
-import { useRouter } from 'next/navigation'
-import React from 'react' 
-import { IoAdd } from 'react-icons/io5'
+import Fundpaystack from '@/components/settings_component/payment_component/card_tabs/fund_wallet/fundpaystack'
+import usePaystackStore from '@/global-state/usePaystack'
+import useSearchStore from '@/global-state/useSearchData'
+import useCustomTheme from '@/hooks/useTheme'
+import { Box, Flex, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react'
+import { usePathname, useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+import { IoAdd, IoSearchOutline } from 'react-icons/io5'
 
 export default function Donation() {
 
     const {
-        primaryColor, 
+        primaryColor,
         secondaryBackgroundColor,
         borderColor,
-        headerTextColor, 
-    } = useCustomTheme() 
+        headerTextColor,
+        bodyTextColor
+    } = useCustomTheme()
 
     const { configPaystack, setPaystackConfig, donation, dataID } = usePaystackStore((state) => state);
     const router = useRouter()
 
+    const { search, setSearchValue } = useSearchStore((state) => state);
+
+    const path = usePathname()
+
+    useEffect(() => {
+        setSearchValue("")
+    }, [path])
+
     return (
-        <Flex w={"full"} px={"6"} py={"12"} flexDir={"column"} overflowY={"auto"} >
-            <Flex pb={"2"} w={"full"} h={"fit-content"} flexDirection={["column", "row", "row", "row"]} gap={"2"} justifyContent={"space-between"} borderBottomWidth={"1px"} borderColor={borderColor} >
+        <Flex w={"full"} px={"6"} pt={["6", "6", "12", "12"]} pb={"12"} flexDir={"column"} overflowY={"auto"} >
+            <Flex pb={"2"} w={"full"} h={"fit-content"} flexDirection={["column", "column", "row", "row"]} gap={"2"} justifyContent={"space-between"} borderBottomWidth={"1px"} borderColor={borderColor} >
                 <Text fontSize={"24px"} fontWeight={"700"} ><span style={{ color: primaryColor }} >Chasescroll</span> Fundraising</Text>
+                <Box display={["flex", "flex", "none", "none"]} >
+                    <InputGroup width={["full", "full", "361px"]} zIndex={"20"} position={"relative"} >
+                        <InputLeftElement pointerEvents='none'>
+                            <IoSearchOutline size={"25px"} />
+                        </InputLeftElement>
+                        <Input width={["full", "full", "361px"]} value={search} onChange={(e) => setSearchValue(e.target.value)} type="search" borderColor={borderColor} rounded={"12px"} focusBorderColor={'brand.chasescrollBlue'} _placeholder={{ color: bodyTextColor }} bgColor={secondaryBackgroundColor} placeholder={'Search for fundraising'} />
+                    </InputGroup>
+                </Box>
                 <CustomButton onClick={() => router?.push("/dashboard/donation/create")} text={"Create Fundraising"} px={"4"} height={"45px"} fontSize={"sm"} borderRadius={"32px"} fontWeight={"600"} width={"fit-content"} />
             </Flex>
-            <Flex py={"4"} pb={"8"} gap={["4", "4", "6", "6"]} flexDir={["column", "column", "row", "row"]} alignItems={["start", "start", "center", "center"]} >
+            <Flex py={"4"} pb={"8"} gap={["4", "4", "6", "6"]} flexDir={["column", "row", "row", "row"]} alignItems={["start", "center", "center", "center"]} >
                 <Text >Fundraising Campaign ongoing </Text>
-                <CustomButton mt={["2", "2", "0px", "0px"]} text={"New fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={"#F6F7FA"} borderRadius={"32px"} fontWeight={"600"} color={primaryColor} width={"fit-content"} />
-                <CustomButton onClick={()=> router?.push("/dashboard/donation/mydonation")} text={"My fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={secondaryBackgroundColor} borderRadius={"32px"} fontWeight={"600"} color={headerTextColor} width={"fit-content"} />
+                <Flex  mt={["2", "0px", "0px", "0px"]} flexDir={["row", "row", "row", "row"]} alignItems={"center"} gap={["4", "4", "6", "6"]}>
+                    <CustomButton text={"New fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={"#F6F7FA"} borderRadius={"32px"} fontWeight={"600"} color={primaryColor} width={"fit-content"} />
+                    <CustomButton onClick={() => router?.push("/dashboard/donation/mydonation")} text={"My fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={secondaryBackgroundColor} borderRadius={"32px"} fontWeight={"600"} color={headerTextColor} width={"fit-content"} />
+                </Flex>
             </Flex>
-            <DonationItemList />
+            <DonationItemList publicData={true} />
             <Fundpaystack id={dataID} config={configPaystack} setConfig={setPaystackConfig} donation={donation} />
         </Flex>
     )
