d848680396d53b2c9438337d1078c90d0b661c32
ekadaegile <egileoniso.ekada@gmail.com>
2025-01-24 21:11:40 +0100

    update on donation

---
+++ src/components/donation/donationItemGroup.tsx
@@ -0,0 +1,179 @@
+import useGetDonationList from '@/hooks/useGetDonationList'
+import { IMAGE_URL } from '@/services/urls'
+import { capitalizeFLetter } from '@/utils/capitalLetter'
+import { textLimit } from '@/utils/textlimit'
+import { Grid, GridItem, Flex, Text, Image } from '@chakra-ui/react'
+import moment from 'moment'
+import React, { useState } from 'react'
+import { IoClose, IoInformationCircleOutline } from 'react-icons/io5'
+import CustomButton from '../general/Button'
+import LoadingAnimation from '../sharedComponent/loading_animation'
+import UserImage from '../sharedComponent/userimage'
+import DonationGraph from './donationGraph'
+import useCustomTheme from '@/hooks/useTheme'
+import { IDonationGroup, IDonationList } from '@/models/donation'
+import useGetDonationGroup from '@/hooks/useGetDonationGroup'
+import ModalLayout from '../sharedComponent/modal_layout'
+import { useRouter } from 'next/navigation'
+import DonationGroupModal from './donationGroupModal'
+import { dateFormat } from '@/utils/dateFormat'
+import DonationPayment from './donationPayment'
+import DonationBtn from './donationBtn'
+import ShareEvent from '../sharedComponent/share_event'
+import InfiniteScrollerComponent from '@/hooks/infiniteScrollerComponent'
+import DeleteEvent from '../sharedComponent/delete_event'
+import useSearchStore from '@/global-state/useSearchData'
+import BlurredImage from '../sharedComponent/blurred_image'
+
+
+
+export default function DonationItemGroup({ details, singleData, creator, pasted }: { details?: boolean, singleData?: IDonationList, creator?: boolean, publicData?: boolean, pasted?: boolean }) {
+
+    const {
+        bodyTextColor,
+        borderColor, 
+        mainBackgroundColor
+    } = useCustomTheme()
+
+
+    const { search } = useSearchStore((state) => state);
+
+
+    const userId = localStorage.getItem('user_id') + "";
+    const [selected, setSelected] = useState({} as IDonationList)
+    // const { data: groupData, isLoading: loading, isRefetching } = useGetDonationGroup(singleData?.fundRasingGroupId?.id)
+    const [open, setOpen] = useState(false)
+
+    const { results, isLoading: loadingList, ref, isRefetching: refetchingList } = InfiniteScrollerComponent({ url:`/fund-raiser-group/search${search ? `?name=${search.toLowerCase()}` : ``}`, limit: 20, filter: "id", name: "donationlist", search: search })
+
+    const router = useRouter()
+
+    const clickHander = (item: IDonationGroup, index: string) => {
+        if(creator || pasted){
+            router?.push("/dashboard/donation/" + index)
+        } else if (item?.fundRaisers?.length > 1) {
+            // setSelected(item)
+            router?.push("/dashboard/donation/group/" + index)
+        } else {
+            router?.push("/dashboard/donation/" + item?.fundRaisers[0]?.id)
+        }
+    }
+
+    console.log(results);
+    
+
+    return (
+        <Flex w={"full"} flexDir={"column"} gap={"5"} >
+            {!details && (
+                <LoadingAnimation loading={loadingList} refeching={refetchingList} length={results?.length} withimg={true} >
+                    <Grid w={"full"} templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']} gap={6} >
+                        {results?.map((item: IDonationGroup, index: number) => {
+                            if (results?.length === index + 1) {
+                                return (
+                                    // <GridItem w={"full"}  >
+                                    <Flex w={"full"} height={"fit-content"} ref={ref} key={index} pos={"relative"} gap={"4"} flexDir={"column"} p={"4"} borderWidth={"1px"} borderColor={borderColor} rounded={"16px"} >
+                                        <Flex w={"full"} pos={"relative"} alignItems={"center"} justifyContent={"space-between"} >
+                                            <Flex as={"button"} w={"fit-content"} alignItems={"center"} onClick={() => router?.push(`/dashboard/profile/${item?.user?.userId}`)} gap={"3"} >
+                                                <UserImage size={"45px"} font={"20px"} data={item?.user} image={item?.user?.data?.imgMain?.value} border={"1px"} />
+                                                <Flex display={["none", "none", "block"]} flexDir={"column"} textAlign={"left"}  >
+                                                    <Text color={"#233DF3"} fontSize={"14px"} fontWeight={"600"} >{textLimit(capitalizeFLetter(item?.user?.firstName) + " " + capitalizeFLetter(item?.user?.lastName), 15)}</Text>
+                                                    <Text fontSize={"12px"} color={bodyTextColor} >{dateFormat(item?.user)}</Text>
+                                                </Flex>
+                                            </Flex>
+                                            <DeleteEvent donation={true} event={item} />
+                                        </Flex>
+                                        <Flex as={"button"} onClick={() => clickHander(item, item?.fundRaisers[0]?.id)} w={'full'} h={"200px"} pos={"relative"} rounded={"8px"} >
+
+                                            <BlurredImage height={["200px"]} image={item?.fundRaisers[0]?.bannerImage} />
+                                            {/* <Image rounded={"8px"} objectFit="cover" alt={item?.name} width={"full"} height={"full"} src={IMAGE_URL + item?.bannerImage} /> */}
+                                        </Flex>
+                                        <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"} >
+                                            <Flex flexDir={"column"} >
+                                                <Text fontSize={"14px"} color={bodyTextColor} >Fund Raising Title</Text>
+                                                <Text fontWeight={"700"} >{textLimit(capitalizeFLetter(item?.name), 35)}</Text>
+                                            </Flex>
+                                            <ShareEvent newbtn={true} showText={false} data={item} id={item?.id} type="EVENT" eventName={textLimit(item?.name, 17)} />
+                                        </Flex>
+                                        <Flex w={"full"} borderWidth={(item?.fundRasingGroupId?.id && !creator && !pasted) ? "1px" : "0px"} borderColor={borderColor} rounded={"8px"} py={"7px"} px={"8px"} justifyContent={"space-between"} >
+                                            {(item?.fundRasingGroupId?.id && !creator && !pasted) && (
+                                                <Flex gap={"1"} alignItems={"center"} >
+                                                    <IoInformationCircleOutline />
+                                                    <Text fontSize={"12px"} >{item?.totalInGroup - 1} More fundraising available  </Text>
+                                                </Flex>
+                                            )}
+                                            <CustomButton ml={"auto"} onClick={() => clickHander(item, item?.fundRaisers[0]?.id)} text={"View"} px={"6"} borderRadius={"32px"} width={"fit-content"} height={"29px"} fontSize={"sm"} />
+                                        </Flex> 
+                                        <DonationGraph item={item?.fundRaisers[0]} />
+                                        {(userId !== item?.user?.userId && !pasted) && (
+                                            <DonationBtn {...item} />
+                                        )}
+                                    </Flex>
+                                    // </GridItem>
+                                )
+                            } else {
+                                return (
+                                    // <GridItem w={"full"} key={index} >
+                                    <Flex w={"full"} height={"fit-content"} key={index} gap={"4"} pos={"relative"} flexDir={"column"} p={"4"} borderWidth={"1px"} borderColor={borderColor} rounded={"16px"} >
+                                        <Flex w={"full"} pos={"relative"} alignItems={"center"} justifyContent={"space-between"} >
+                                            <Flex as={"button"} w={"fit-content"} alignItems={"center"} onClick={() => router?.push(`/dashboard/profile/${item?.createdBy?.userId}`)} gap={"3"} >
+                                                <UserImage size={"45px"} font={"20px"} data={item?.user} image={item?.user?.data?.imgMain?.value} border={"1px"} />
+                                                <Flex display={["none", "none", "block"]} flexDir={"column"} textAlign={"left"}  >
+                                                    <Text color={"#233DF3"} fontSize={"14px"} fontWeight={"600"} >{textLimit(capitalizeFLetter(item?.user?.firstName) + " " + capitalizeFLetter(item?.user?.lastName), 15)}</Text>
+                                                    <Text fontSize={"12px"} color={bodyTextColor} >{dateFormat(item?.user)}</Text>
+                                                </Flex>
+                                            </Flex>
+                                            <DeleteEvent donation={true} event={item} />
+                                        </Flex>
+                                        <Flex as={"button"} onClick={() => clickHander(item, item?.fundRaisers[0]?.id)} w={'full'} h={"200px"} rounded={"8px"} >
+
+                                            <BlurredImage height={["200px"]} image={item?.fundRaisers[0]?.bannerImage} />
+                                            {/* <Image rounded={"8px"} objectFit="cover" alt={item?.name} width={"full"} height={"full"} src={IMAGE_URL + item?.bannerImage} /> */}
+                                        </Flex>
+                                        <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"} >
+                                            <Flex flexDir={"column"} >
+                                                <Text fontSize={"14px"} color={bodyTextColor} >Fund Raising Title</Text>
+                                                <Text fontWeight={"700"} >{textLimit(capitalizeFLetter(item?.name), 35)}</Text>
+                                            </Flex>
+                                            <ShareEvent newbtn={true} showText={false} data={item} id={item?.id} type="EVENT" eventName={textLimit(item?.name, 17)} />
+                                        </Flex>
+                                        <Flex w={"full"} borderWidth={(item?.fundRaisers?.length > 1 && !creator && !pasted) ? "1px" : "0px"} borderColor={borderColor} rounded={"8px"} py={"7px"} px={"8px"} justifyContent={"space-between"} >
+                                            {(item?.fundRaisers?.length > 1 && !creator && !pasted) && (
+                                                <Flex gap={"1"} alignItems={"center"} >
+                                                    <IoInformationCircleOutline />
+                                                    <Text fontSize={"12px"} >{item?.fundRaisers?.length} More fundraising available  </Text>
+                                                </Flex>
+                                            )}
+                                            <CustomButton ml={"auto"} onClick={() => clickHander(item, item?.fundRaisers[0]?.id)} text={"View"} px={"6"} borderRadius={"32px"} width={"fit-content"} height={"29px"} fontSize={"sm"} />
+                                        </Flex>
+                                        <DonationGraph item={item?.fundRaisers[0]} />
+                                        {(userId !== item?.user?.userId && !pasted) && (
+                                            <DonationBtn {...item} />
+                                        )}
+                                    </Flex>
+                                    // </GridItem>
+                                )
+                            }
+                        })}
+                    </Grid>
+                </LoadingAnimation>
+            )} 
+
+            <ModalLayout open={open} close={setOpen} size={"xl"} >
+                <Flex flexDir={"column"} w={"full"} p={"5"} pt={"0px"} pos={"relative"} >
+                    <Flex position={"relative"} zIndex={"50"} w={"full"} justifyContent={"space-between"} pt={"5"} pb={"2"} bgColor={mainBackgroundColor} pos={"sticky"} top={"0px"} gap={"3"} alignItems={"center"} >
+                        <Flex flexDirection={"column"} >
+                            <Text fontSize={"14px"} color={bodyTextColor} >Chasescroll Fundraising</Text>
+                            <Text fontWeight={"600"} >Fundraising Available in {selected?.name}</Text>
+                        </Flex>
+                        <Flex as={"button"} onClick={() => setOpen(false)} >
+                            <IoClose size="20px" color={bodyTextColor} />
+                        </Flex>
+                    </Flex>
+                    <Flex mt={"6"} flexDirection={"column"} gap={"4"} >
+                        <DonationGroupModal selectedData={selected} />
+                    </Flex>
+                </Flex>
+            </ModalLayout>
+        </Flex>
+    )
+}
--- src/components/new_home_component/bottomSheetComment.tsx
+++ src/components/new_home_component/bottomSheetComment.tsx
@@ -3,8 +3,6 @@ import { Flex, Spinner, Text } from '@chakra-ui/react'
 import { count } from 'console'
 import React, { useEffect, useRef, useState } from 'react'
 import { Sheet } from 'react-modal-sheet'
-import ShareBtn from '../sharedComponent/new_share_btn'
-import { HomeHeartIcon, HomeHeartFillIcon, HomeCommentIcon } from '../svg'
 import CommentInput from './commentInput'
 import CommentList from './commentList'
 import { IMediaContent } from '@/models/MediaPost'
@@ -31,20 +29,80 @@ export default function BottomSheetComment({ open, setOpen, content, liked, load
     const [show, setShow] = useState(true)
     const [replyData, setReplyData] = useState({} as any)
 
+    const containerRef: any = useRef(null);
+
+    const scrollToInput = (event: any) => {
+        console.log("scrollToInput", event)
+        const inputElement = event.target;
+        const container = containerRef.current;
+
+        if (container && inputElement) {
+            const inputRect = inputElement.getBoundingClientRect();
+            const containerRect = container.getBoundingClientRect();
+
+            // Check if the input is out of view
+            if (inputRect.bottom > containerRect.bottom || inputRect.top < containerRect.top) {
+                container.scrollTo({
+                    top: inputElement.offsetTop - container.offsetTop,
+                    behavior: "smooth",
+                });
+            }
+        }
+    };
+
     return (
-        <Sheet isOpen={open} onClose={() => setOpen(false)}>
-            <Sheet.Container style={{ backgroundColor: mainBackgroundColor }} >
+        <Sheet isOpen={open}
+            detent="content-height"
+            onClose={() => setOpen(false)}>
+            <Sheet.Container
+                style={{
+                    backgroundColor: mainBackgroundColor,
+                    height: '100%',
+                    position: 'relative'
+                }}
+            >
                 <Sheet.Header />
                 <Sheet.Content>
-                    <Flex w={"full"} flexDir={"column"} position={"relative"} h={"full"} >
+                    <Flex
+                        w={"full"}
+                        flexDir={"column"}
+                        position={"relative"}
+                        ref={containerRef}
+                        h={"full"}
+                        pb={"80px"} // Add padding to ensure content is visible above keyboard
+                    >
                         <Flex w={"full"} h={"80px"} position={"sticky"} borderBottomWidth={"1px"} borderBottomColor={borderColor} top={"0px"} justifyContent={"center"} alignItems={"center"} >
                             <Text fontWeight={"500"} fontSize={"16px"} >Comments</Text>
                         </Flex>
                         <CommentList user={user} mobile={true} replyData={replyData} setReply={setReplyData} data={content} showInput={setShow} />
-                        {/* {show && ( */} 
-                            <Flex w={"full"} h={"fit-content"} bg={mainBackgroundColor} zIndex={"30"} position={"sticky"} borderTopColor={borderColor} borderTopWidth={"1px"} bottom={"0px"} pt={"2"} pb={"3"} flexDir={"column"} gap={"0px"} alignItems={"start"} >
-                                <CommentInput open={open} setShow={setShow} replyData={replyData} data={content} user={user} setReplyData={setReplyData} />
-                            </Flex> 
+                        <Flex
+                            w={"full"}
+                            mt={"auto"}
+                            h={"fit-content"}
+                            bg={mainBackgroundColor}
+                            zIndex={"30"}
+                            position={"fixed"} // Change to fixed positioning
+                            borderTopColor={borderColor}
+                            borderTopWidth={"1px"}
+                            bottom={"0px"}
+                            left={"0px"} // Ensure proper alignment
+                            right={"0px"} // Ensure proper alignment
+                            pt={"2"}
+                            pb={"3"}
+                            flexDir={"column"}
+                            gap={"0px"}
+                            alignItems={"start"}
+                        >
+                            <CommentInput
+                            handleFocus={scrollToInput}
+                                open={open}
+                                setShow={setShow}
+                                replyData={replyData}
+                                data={content}
+                                user={user}
+                                setReplyData={setReplyData}
+                            />
+                        </Flex>
                     </Flex>
                 </Sheet.Content>
             </Sheet.Container>
