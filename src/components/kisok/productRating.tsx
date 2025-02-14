a1d036c1bc919606e6214bcc48bccd0645768de0
ekadaegile <egileoniso.ekada@gmail.com>
2025-02-14 15:30:09 +0100

    update on product

--- src/components/kisok/productRating.tsx
+++ src/components/kisok/productRating.tsx
@@ -6,12 +6,32 @@ import ReviewData from './reviewData'
 import { FaRegStar, FaStar } from 'react-icons/fa'
 import { useQuery } from 'react-query'
 import httpService from '@/utils/httpService'
+import { IUser } from '@/models/User'
+import UserImage from '../sharedComponent/userimage'
+import { dateFormat } from '@/utils/dateFormat'
+
+
+interface IReview {
+    "id": any,
+    "createdDate": number,
+    "lastModifiedBy": any,
+    "createdBy": any,
+    "lastModifiedDate": number,
+    "isDeleted": boolean,
+    "status": any,
+    "statusCode": number,
+    "returnMessage": string,
+    "user": IUser
+    "description": string,
+    "rating": number,
+    "reviewType": string
+}
 
 export default function ProductRating({ item }: { item: string }) {
 
     const { borderColor, primaryColor } = useCustomTheme()
 
-    const [data, setItem] = useState({} as any) 
+    const [data, setItem] = useState<Array<IReview>>([])
 
     const { isLoading } = useQuery(
         ["review"],
@@ -22,8 +42,8 @@ export default function ProductRating({ item }: { item: string }) {
         }), {
         onSuccess(data) {
             console.log(data);
-            
-            // setItem(data?.data?.content[0])
+
+            setItem(data?.data?.content)
         }
     });
 
@@ -36,35 +56,40 @@ export default function ProductRating({ item }: { item: string }) {
                     <Flex w={"fit-content"} >
                         <Flex w={"8px"} h={"8px"} rounded={"full"} bgColor={borderColor} />
                     </Flex>
-                    <Text fontWeight={"700"}>234 Recommendations</Text>
+                    <Text fontWeight={"700"}>{data?.length} Recommendations</Text>
                     <Text fontSize={"14px"} w={"120px"} fontWeight={"500"} ml={"9"} >See Review</Text>
                     <ReviewData item={item} />
                 </Flex>
             </Flex>
             <Flex flexDirection={"column"} pt={"6"} gap={"3"} >
-                <Flex flexDir={"column"} gap={"2"} >
-                    <Flex alignItems={"center"} gap={"2"} >
-                        <Flex rounded={"30px"} roundedTopRight={"0px"} h={"41px"} w={"41px"} bgColor={"blue"} />
-                        <Flex flexDir={"column"} >
-                            <Text fontWeight={"600"} fontSize={"14px"} >Lena Kyles</Text>
-                            <Flex flexDir={"row"} gap={"1"} >
-                                {[1, 2, 3, 4, 5]?.map((item) => {
-                                    return (
-                                        <Flex key={item} >
-                                            {(item > 3) ? (
-                                                <FaRegStar size={"15px"} />
-                                            ) : (
-                                                <FaStar color='#FBBD08' size={"15px"} />
-                                            )}
-                                        </Flex>
-                                    )
-                                })}
+                {data?.map((item, index) => {
+                    return (
+                        <Flex key={index} flexDir={"column"} gap={"2"} >
+                            <Flex alignItems={"center"} gap={"2"} >
+                                {/* <Flex rounded={"30px"} roundedTopRight={"0px"} h={"41px"} w={"41px"} bgColor={"blue"} /> */}
+                                <UserImage border={"1px"} image={item?.user?.data?.imgMain?.value} data={item?.user} size={"41px"} />
+                                <Flex flexDir={"column"} >
+                                    <Text fontWeight={"600"} fontSize={"14px"} >Lena Kyles</Text>
+                                    <Flex flexDir={"row"} gap={"1"} >
+                                        {[1, 2, 3, 4, 5]?.map((itemNumb) => {
+                                            return (
+                                                <Flex key={itemNumb} >
+                                                    {(itemNumb > item?.rating) ? (
+                                                        <FaRegStar size={"15px"} />
+                                                    ) : (
+                                                        <FaStar color='#FBBD08' size={"15px"} />
+                                                    )}
+                                                </Flex>
+                                            )
+                                        })}
+                                    </Flex>
+                                    <Text fontSize={"12px"} >{dateFormat(item?.createdDate)}</Text>
+                                </Flex>
                             </Flex>
-                            <Text fontSize={"12px"} >May 03, 2023, 12:56</Text>
+                            <Text fontSize={"14px"} >{item?.description}</Text>
                         </Flex>
-                    </Flex>
-                    <Text fontSize={"14px"} >Lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet cons, Lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet con, Lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet cons, lorem ipsum dolor sit amet con</Text>
-                </Flex>
+                    )
+                })}
             </Flex>
         </Flex>
     )
