e35871be6d56a8cd6d63e89cc2d7e4647c33321a
ekadaegile <egileoniso.ekada@gmail.com>
2025-02-17 13:38:56 +0100

    update on font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Providers } from './Provider'
 import './globals.css';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/pages/api/auth/[...nextauth]'; 
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { GoogleAuthProvider } from '@/utils/GoogleProvider';
 
 const APP_NAME = "Chasescroll";
@@ -32,7 +32,7 @@ export default async function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon_io/site.webmanifest" />  
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
         <meta name="viewport" content="minimum-scale=1" />
         <meta name="viewport" content="height=device-height, 
                       width=device-width, initial-scale=1.0,
@@ -40,8 +40,11 @@ export default async function RootLayout({
                       minimum-scale=1.0, maximum-scale=1.0, 
                       user-scalable=no, target-densitydpi=device-dpi" />
         {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       </head>
-      <body> 
+      <body style={{ fontFamily: "Raleway" }} >
         <GoogleAuthProvider>
           <Providers session={session}>
             {children}
