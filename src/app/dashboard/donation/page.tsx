6be64a1d4265ce451e6c9f05aa8e707aa5fd61cc
ekadaegile <egileoniso.ekada@gmail.com>
2025-02-17 15:20:12 +0100

    update on fundraising

--- src/app/dashboard/donation/page.tsx
+++ src/app/dashboard/donation/page.tsx
@@ -4,6 +4,7 @@ import DonationItemList from '@/components/donation/donationItemList'
 import SelectDonationPage from '@/components/donation/select_donation_page'
 import CustomButton from '@/components/general/Button'
 import Fundpaystack from '@/components/settings_component/payment_component/card_tabs/fund_wallet/fundpaystack'
+import { FundraisingIcon } from '@/components/svg'
 import useDonationStore from '@/global-state/useDonationState'
 import usePaystackStore from '@/global-state/usePaystack'
 import useSearchStore from '@/global-state/useSearchData'
@@ -32,7 +33,7 @@ export default function Donation() {
 
     useEffect(() => {
         setSearchValue("")
-    }, [path]) 
+    }, [path])
 
     const { updateDontion } = useDonationStore((state) => state)
 
@@ -54,8 +55,12 @@ export default function Donation() {
 
     return (
         <Flex w={"full"} px={"6"} pt={["6", "6", "12", "12"]} pb={"12"} flexDir={"column"} overflowY={"auto"} >
-            <Flex pb={"2"} w={"full"} h={"fit-content"} flexDirection={["column", "column", "row", "row"]} gap={"2"} justifyContent={"space-between"} borderBottomWidth={"1px"} borderColor={borderColor} >
-                <Text fontSize={"24px"} fontWeight={"700"} ><span style={{ color: primaryColor }} >Chasescroll</span> Fundraising</Text>
+            <Flex pb={"2"} w={"full"} h={"fit-content"} flexDirection={["column", "column", "row", "row"]} gap={"2"} justifyContent={"space-between"} borderBottomWidth={"1px"} borderColor={borderColor} alignItems={"center"} >
+                {/* */}
+                <Flex alignItems={"center"} gap="2" >
+                    <FundraisingIcon />
+                    <Text fontSize={"24px"} fontWeight={"700"} >Fundraising</Text>
+                </Flex>
                 <Box display={["flex", "flex", "none", "none"]} >
                     <InputGroup width={["full", "full", "361px"]} zIndex={"20"} position={"relative"} >
                         <InputLeftElement pointerEvents='none'>
@@ -67,11 +72,11 @@ export default function Donation() {
                 <CustomButton onClick={() => router?.push("/dashboard/donation/create")} text={"Create Fundraising"} px={"4"} height={"45px"} fontSize={"sm"} borderRadius={"32px"} fontWeight={"600"} width={"fit-content"} />
             </Flex>
             <Flex py={"4"} pb={"8"} gap={["4", "4", "6", "6"]} flexDir={["column", "row", "row", "row"]} alignItems={["start", "center", "center", "center"]} >
-                <Flex  mt={["2", "0px", "0px", "0px"]} flexDir={["row", "row", "row", "row"]} alignItems={"center"} gap={["4", "4", "6", "6"]}>
+                <Flex mt={["2", "0px", "0px", "0px"]} flexDir={["row", "row", "row", "row"]} alignItems={"center"} gap={["4", "4", "6", "6"]}>
                     {/* <CustomButton text={"All Fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={"#F6F7FA"} borderRadius={"32px"} fontWeight={"600"} color={primaryColor} width={"fit-content"} />
                     <CustomButton onClick={() => router?.push("/dashboard/donation/mydonation")} text={"My Fundraising"} px={"15px"} height={"40px"} fontSize={"sm"} backgroundColor={secondaryBackgroundColor} borderRadius={"32px"} fontWeight={"600"} color={headerTextColor} width={"fit-content"} /> */}
-                 <SelectDonationPage />
-                 
+                    <SelectDonationPage />
+
                 </Flex>
             </Flex>
             <DonationItemGroup publicData={true} />
