b59a7e9f3999b72f53c2f379e31e654225ffdff2
egileekada <egileoniso.ekada@gmail.com>
2025-06-24 10:53:50 +0100

    update on external page

--- src/app/external/product/[id]/page.tsx
+++ src/app/external/product/[id]/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import GetProduct from "@/components/kisok/getProduce";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function ExternalProduct () {
     return(
-        <Flex w={"full"} justifyContent={"start"} alignItems={"start"} p={"6"} > 
+        <Flex w={"full"} justifyContent={"start"} flexDir={"column"} gap={"6"} alignItems={"start"} p={"6"} >
+            <Text fontSize={"20px"} fontWeight={"600"} >Product</Text>
             <GetProduct myproduct={true} />
         </Flex>
     )
--- src/app/external/services/[id]/page.tsx
+++ src/app/external/services/[id]/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import Businesses from "@/Views/dashboard/booking/Businesses";
 import MyBusiness from "@/Views/dashboard/booking/MyBusiness";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function ExternalService () {
     return(
-        <Flex w={"full"} flexDir={"column"} justifyContent={"start"} alignItems={"start"} p={"6"} > 
+        <Flex w={"full"} justifyContent={"start"} flexDir={"column"} gap={"6"} alignItems={"start"} p={"6"} >
+            <Text fontSize={"20px"} fontWeight={"600"} >Services</Text>
             <MyBusiness />
         </Flex>
     )
